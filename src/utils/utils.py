175b2a4567fb2cdca9ad9512c963071dd35a053e
Nick <nmarnautov@gmail.com>
2026-04-08 20:10:38 +0800

    release v.1.0.0

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,12 +1,17 @@
 import logging
 import logging.config
+from pathlib import Path
+
 import yaml
 
 
 def setup_logging(logging_config_path: str) -> None:
-    if logging_config_path != "default":        
+    if logging_config_path != "default":
         with open(logging_config_path, "r") as f:
             log_config = yaml.safe_load(f)
+            for handler in log_config.get("handlers", {}).values():
+                if "filename" in handler:
+                    Path(handler["filename"]).parent.mkdir(parents=True, exist_ok=True)
             logging.config.dictConfig(log_config)
     else:
         logger = logging.getLogger("binarizer")
